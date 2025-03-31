6dfedbd3f499cf3123b782181059ca43816f9c7d
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 14:39:39 +0800

    check if the length of the string is less than the number, then add zeros at beginning if it is

--- add_zeros_at_start_of_string_not_using_zfill().py
+++ add_zeros_at_start_of_string_not_using_zfill().py
@@ -5,3 +5,9 @@ string = input("Enter a string: ")
 number = int(input("Enter a number: "))
 
 # check if the length of the string is less than the number
+if len(string) < number:
+    zeros_to_add = number - len(string)  # calculate the number of zeros to add
+    string = "0" * zeros_to_add + string   # add zeros at the beginning of the string
+    print("String with added zeros:", string)
+else:
+    print("The length of the string is greater than or equal to the number. No zeros added.")
