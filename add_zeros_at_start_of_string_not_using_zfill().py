5aa7a12ec22b09bfe9d7a6c4885d5236a03bdcac
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 14:39:01 +0800

    ask user to input a string and number

--- add_zeros_at_start_of_string_not_using_zfill().py
+++ add_zeros_at_start_of_string_not_using_zfill().py
@@ -1,4 +1,7 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 # ask user to input a string and a number
+string = input("Enter a string: ")
+number = int(input("Enter a number: "))
+
 # check if the length of the string is less than the number
