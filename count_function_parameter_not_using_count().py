f5627fcb2a575ad3a675cfed11b232eb75a248b5
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 14:45:23 +0800

    ask user to input a string

--- count_function_parameter_not_using_count().py
+++ count_function_parameter_not_using_count().py
@@ -1,6 +1,8 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 # ask user to input a string
+string = input("Enter a string: ")
+
 # ask user to input a character to count
 # initialize a variable to count the number of times the character appears in the string
 # loop through each character in the string
