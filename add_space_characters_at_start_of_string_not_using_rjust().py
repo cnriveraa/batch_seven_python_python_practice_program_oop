1d0c107544cac493e0cfa03efd10593f042a244f
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 14:30:43 +0800

    ask user to input a string

--- add_space_characters_at_start_of_string_not_using_rjust().py
+++ add_space_characters_at_start_of_string_not_using_rjust().py
@@ -1,6 +1,8 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
 # ask user to input a string
+string = input("Enter a string: ")
+
 # ask user to input the number of characters
 # check if the length of the string is less than the number of characters
 # print the new string
