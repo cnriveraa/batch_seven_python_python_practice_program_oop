f8f430ae3bbbb40e69dca8355f99fad7904e9d73
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 14:52:46 +0800

    ask user to input a string

--- find_first_location_of_function_parameter_not_using_index().py
+++ find_first_location_of_function_parameter_not_using_index().py
@@ -1,6 +1,8 @@
 # Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 # ask user to input a string
+string = input("Enter a string: ")
+
 # ask user to input a character to find its first location in the string
 # initialize a variable to store the first location of the character in the string
 # loop through the string to find the first location of the character
