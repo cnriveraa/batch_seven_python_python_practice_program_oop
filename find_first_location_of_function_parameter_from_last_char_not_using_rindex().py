fa082d85bf69763c1f2ee91b8df2549fda5583e3
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 15:01:09 +0800

    ask user to input a string

--- find_first_location_of_function_parameter_from_last_char_not_using_rindex().py
+++ find_first_location_of_function_parameter_from_last_char_not_using_rindex().py
@@ -1,6 +1,8 @@
 # Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 
 # ask user to input a string
+string = input("Enter a string: ")
+
 # ask user to input a character to find
 # initialize the index to -1
 # loop through the string in reverse order
