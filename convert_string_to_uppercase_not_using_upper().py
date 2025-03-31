8e101930fee28565aa30904a8dd0eebeea32758a
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 13:56:59 +0800

    pseudocode added

---
+++ convert_string_to_uppercase_not_using_upper().py
@@ -0,0 +1,6 @@
+# Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+# ask user to input a string
+# create an empty string to store the result
+# iterate through each character in the string
+# print the result
