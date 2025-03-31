381e6c08b6821d5db3b0f2004d26ccdaa6ab6356
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 15:01:00 +0800

    pseudocode added

---
+++ find_first_location_of_function_parameter_from_last_char_not_using_rindex().py
@@ -0,0 +1,7 @@
+# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+
+# ask user to input a string
+# ask user to input a character to find
+# initialize the index to -1
+# loop through the string in reverse order
+# print the result
