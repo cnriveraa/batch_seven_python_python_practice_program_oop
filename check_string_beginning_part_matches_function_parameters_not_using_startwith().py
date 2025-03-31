872805bf0ae4d088bb5828d201856a968d834d3f
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 14:20:12 +0800

    pseudocode added

---
+++ check_string_beginning_part_matches_function_parameters_not_using_startwith().py
@@ -0,0 +1,5 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# ask user to input a string
+# ask user to input a prefix
+# check if the string starts with the prefix
