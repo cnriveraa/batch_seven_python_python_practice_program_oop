7407a3ee4fce4e23a108cf47e19e1e17e15e1539
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 13:57:25 +0800

    ask user to input a string

--- convert_string_to_uppercase_not_using_upper().py
+++ convert_string_to_uppercase_not_using_upper().py
@@ -1,6 +1,8 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 # ask user to input a string
+string = input("Enter a string: ")
+
 # create an empty string to store the result
 # iterate through each character in the string
 # print the result
