62389ebb93cb3b9baa2dfe01d132299ab5758474
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 14:20:27 +0800

    ask user to input a string

--- check_string_beginning_part_matches_function_parameters_not_using_startwith().py
+++ check_string_beginning_part_matches_function_parameters_not_using_startwith().py
@@ -1,5 +1,7 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 # ask user to input a string
+string = input("Enter a string: ")
+
 # ask user to input a prefix
 # check if the string starts with the prefix
