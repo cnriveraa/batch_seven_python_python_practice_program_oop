1d140227b96749dabbaae2de24c5261c5d50873e
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 14:31:54 +0800

    check if the length of the string is less than the number of characters entered

--- add_space_characters_at_start_of_string_not_using_rjust().py
+++ add_space_characters_at_start_of_string_not_using_rjust().py
@@ -7,4 +7,11 @@ string = input("Enter a string: ")
 number_of_characters = int(input("Enter the number of characters: "))
 
 # check if the length of the string is less than the number of characters
+if len(string) < number_of_characters:
+    spaces = number_of_characters - len(string)    # calculate the number of spaces needed
+    spaces_string = " " * spaces   # create a string with the number of spaces needed
+    new_string = spaces_string + string   # add the spaces to the beginning of the string
+else:
+    new_string = string
+
 # print the new string
