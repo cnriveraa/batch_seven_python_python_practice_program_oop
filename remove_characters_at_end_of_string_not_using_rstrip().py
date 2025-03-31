d16bd821300d1eeb1600d8f104de87632f5c5e6b
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 13:32:21 +0800

    ask user to input a string

--- remove_characters_at_end_of_string_not_using_rstrip().py
+++ remove_characters_at_end_of_string_not_using_rstrip().py
@@ -1,5 +1,7 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
 # ask user to input a string
+string = input("Enter a string: ")
+
 # remove space at end of string
 # print the result
