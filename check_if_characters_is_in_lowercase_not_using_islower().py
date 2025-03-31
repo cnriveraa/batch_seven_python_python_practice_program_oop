c0ec39a7bbb7526c2b86bab4fbc9d2012c771e9c
cnriveraa <rivera.cn18@gmail.com>
2025-03-31 14:13:13 +0800

    ask user to input a string

--- check_if_characters_is_in_lowercase_not_using_islower().py
+++ check_if_characters_is_in_lowercase_not_using_islower().py
@@ -1,5 +1,7 @@
 # Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
 
 # ask user to input a string
+string = input("Enter a string: ")
+
 # check if all characters are in lowercase
 # print the result
